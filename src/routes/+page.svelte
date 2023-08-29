c14c5ab120840d2ee73e7c9cae00cdc9499f580a
itouoti12 <white.sleipnir.jazz@gmail.com>
2023-08-29 19:43:26 +0900

    検証中

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,6 +46,9 @@
         isDispMain = true;
       }, 5000);
     }, 300);
+
+    console.log(myPhoto)
+    console.log(qiitaLogo)
   });
 
   afterUpdate(() => {
@@ -238,6 +241,7 @@
   <section class="w-6/12 my-auto mx-auto py-40" bind:this={pageElements['about_detail']}>
     <h2 class="text-4xl" bind:this={portforioTexts['about_name'].el}>{portforioTexts['about_name'].jp}</h2>
     <img class="mt-5 w-full" src={myPhoto} alt="myphoto" />
+    <img class="mt-5 w-full" src={qiitaLogo} alt="qiita" />
     <p class="pt-10" bind:this={portforioTexts['about_desc1'].el}>
       {portforioTexts['about_desc1'].jp}
     </p>
